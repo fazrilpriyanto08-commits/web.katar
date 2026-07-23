f8c564f815495036791c84d6c26620c65e3a8e7b
Fazril Priyanto <fazrilpriyanto08@gmail.com>
2026-07-24 01:13:56 +0700

    Fix migration syntax and run fresh database seed

--- database/migrations/2026_07_20_102905_create_pendaftars_table.php
+++ database/migrations/2026_07_20_102905_create_pendaftars_table.php
@@ -13,13 +13,9 @@ public function up(): void
 {
     Schema::create('pendaftars', function (Blueprint $table) {
         $table->id();
+        $table->unsignedBigInteger('lomba_id');
         $table->string('nama_warga');
         $table->string('nomor_hp');
-        $table->string('rt_rw'); // Asal RT/RW pendaftar
-        
-        // Menghubungkan pendaftar dengan id lomba yang dipilih
-        $table->foreignId('lomba_id')->constrained('lombas')->onDelete('cascade');
-        
         $table->timestamps();
     });
 }
