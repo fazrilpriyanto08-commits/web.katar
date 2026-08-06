10667874fe3967f93d5757bad7b698b7901139fd
Fazril Priyanto <fazrilpriyanto08@gmail.com>
2026-08-06 23:51:53 +0700

    fix: correct model namespace for Keuangan

--- app/Models/Keuangan.php
+++ app/Models/Keuangan.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Factories\HasFactory;
-use Illuminate\Database\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Keuangan extends Model
 {
